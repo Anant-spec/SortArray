d1e0a9a1c0bf545287ab004b864f7056fdd99483
Anant Hinge <ananthinge135@gmail.com>
2023-01-30 14:06:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 2, 5, 1, 4 };
+            Console.WriteLine("Origina array:" + string.Join(",", numbers));
+            BubbleSort(numbers);
+            Console.WriteLine("Sorted array:" + string.Join(",", numbers));
+            Console.ReadLine();
+        }
+
+        static void BubbleSort(int[] numbers)
+        {
+            int n = numbers.Length;
+            for (int i = 0; i <n; i++)
+            {
+                for (int j = 0; j <n-1-1; j++)
+                {
+                    if (numbers[j] > numbers[j+1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
